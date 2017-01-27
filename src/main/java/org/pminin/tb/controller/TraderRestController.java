a7b8120bf71af53b1e07605975282188a1b38739
Pavel Minin <pavel.minin@aurea.com>
2017-01-27 16:25:11 +0300

    state command

--- src/main/java/org/pminin/tb/controller/TraderRestController.java
+++ src/main/java/org/pminin/tb/controller/TraderRestController.java
@@ -1,10 +1,15 @@
 package org.pminin.tb.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.pminin.tb.Scheduler;
 import org.pminin.tb.model.Instrument;
 import org.pminin.tb.model.Order;
 import org.pminin.tb.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +19,10 @@ public class TraderRestController {
 
 	@Autowired
 	private AccountService accountService;
-	@Autowired 
+	@Autowired
 	private Scheduler scheduler;
+	@Autowired
+	private Environment env;
 
 	public String getAccountDetails() {
 		return "<p>" + accountService.getAccountDetails().toString() + "</p>";
@@ -24,7 +31,7 @@ public class TraderRestController {
 	private String createOrder() {
 		Order order = new Order();
 		order.setInstrument(new Instrument("EUR_USD"));
-		
+
 		order.setSide("buy");
 		double balance = accountService.getAccountDetails().getBalance();
 		order.setUnits((int) (balance / 100 * 1000));
@@ -32,21 +39,29 @@ public class TraderRestController {
 		order.setTakeProfit(1.075);
 		order.setStopLoss(1.070);
 		order = accountService.createOrder(order);
-		return order != null? order.getId() : "Error occured during order creation";
+		return order != null ? order.getId() : "Error occured during order creation";
 	}
-	
+
 	@RequestMapping("/{command}")
-	public String command(@PathVariable String command){
+	public String command(@PathVariable String command) {
 		switch (command) {
-		case "startwork" : scheduler.startWorkEveryDay();
+		case "startwork":
+			scheduler.startWorkEveryDay();
 			return "Starting work...";
-		case "order": 
+		case "order":
 			return createOrder();
 		case "account":
 			return getAccountDetails();
 		case "reset":
 			scheduler.resetState();
 			return "resetting state...";
+		case "state":
+			try {
+				byte[] encoded = Files.readAllBytes(Paths.get(env.getProperty("logging.path") + "spring.log"));
+				return new String(encoded, "UTF-8");
+			} catch (IOException e) {
+				return e.getMessage();
+			}
 		default:
 			return "Incorrect command";
 		}
